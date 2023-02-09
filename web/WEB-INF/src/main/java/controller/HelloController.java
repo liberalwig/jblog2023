15e335fbf7a263d7f69904fadca6035f7a9e171d
최승은 <liberalfilm@gmail.com>
2023-02-09 14:14:14 +0900

    2023-02-09-Thu-14:14

---
+++ web/WEB-INF/src/main/java/controller/HelloController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloController {
+
+    @GetMapping("index")
+    public void S() {
+
+        return ;
+    }
+
+}
