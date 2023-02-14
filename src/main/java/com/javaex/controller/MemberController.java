f2dd2dd68d8e7499a2109b06013d76e48e390caf
최승은 <liberalfilm@gmail.com>
2023-02-14 12:57:37 +0900

    2023-02-14-Tue-12:57

--- src/main/java/com/javaex/controller/MemberController.java
+++ src/main/java/com/javaex/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.javaex.service.MemberService;
 import com.javaex.vo.MemberVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +14,7 @@ public class MemberController {
     MemberService memberService;
 
     // 회원가입 폼
-    @RequestMapping("joinForm")
+    @RequestMapping("/joinForm")
     public String joinForm() {
         System.out.println("UserController > joinForm()");
 
@@ -23,7 +22,7 @@ public class MemberController {
     }
 
     // 회원가입
-    @RequestMapping("join")
+    @RequestMapping("/join")
     public String join(MemberVo memberVo) {
         System.out.println("MemberController > join()");
         memberService.join(memberVo);
